198d594fc153675a0107e65e32c8fe386c0570aa
Juan Sarmiento <juansarmientog96@gmail.com>
2023-06-05 20:08:48 -0300

    aprendiendo a utilizar pinia con options y composition api

---
+++ src/counter/pages/CounterSetupPage.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { useCounterSetupStore } from '@/store/counter-setup';
+import { storeToRefs } from 'pinia';
+import CounterSetupValue from '@/counter/components/CounterSetupValue.vue';
+
+const counterStore = useCounterSetupStore();
+const { count, squareCount } = storeToRefs(counterStore);
+const { increment, incrementBy, reset } = counterStore;
+
+</script>
+
+<template>
+        <h1>Pinia Setup</h1>
+        <h4>Counter: {{ count }}</h4>
+        <h4>Square: {{ squareCount }}</h4>
+
+        <br>
+        <button @click="increment()">+1</button>
+        <button @click="incrementBy(2)">+2</button>
+        <button @click="incrementBy(5)">+5</button>
+        <button @click="reset()">Reset</button>
+
+        <br>
+        <CounterSetupValue />
+</template>
+
+<style scoped>
+button {
+        padding: .5rem;
+        margin-right: 1rem;
+}
+</style>
---
+++ src/store/counter-setup.ts
@@ -0,0 +1,30 @@
+import { defineStore } from 'pinia';
+import { computed, ref } from 'vue';
+
+
+export const useCounterSetupStore = defineStore('counterSetup', () => {
+
+    const count = ref<number>(0);
+    const lastChanged = ref<Date>();
+
+    const incrementBy = ( value: number ) => {
+        count.value += value;
+        lastChanged.value = new Date();
+    };
+
+    const reset = () => {
+        count.value = 0;
+        lastChanged.value = undefined;
+    }
+
+    return {
+        // State properties
+        count, lastChanged,
+        // Getters
+        squareCount: computed(() => count.value * count.value),
+        // Actions
+        increment: () => incrementBy(1),
+        incrementBy,
+        reset,
+    };
+});
